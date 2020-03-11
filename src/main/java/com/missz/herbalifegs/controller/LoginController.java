8c205c1c047c4dc7d0faaa09c22882f0cafd7a57
JanDanpp <18310383196@163.com>
2020-03-11 13:56:39 +0800

    ceshi

--- src/main/java/com/missz/herbalifegs/controller/LoginController.java
+++ src/main/java/com/missz/herbalifegs/controller/LoginController.java
@@ -44,7 +44,6 @@ public class LoginController {
     }
 //
 //    @GetMapping("/logout")
-
 //    public String logout(HttpSession session){
 //        session.removeAttribute("user");
 //        return "login";
